78c728b80712d57140bd5290623398ec2d12f954
Dennis Roberts <dennis@iplantcollaborative.org>
2012-09-06 16:20:16 -0700

    removed the URL

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject org.iplantc/korma "0.3.0-beta11"
   :description "Tasty SQL for Clojure"
-  :url "http://github.com/ibdknox/korma"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [org.clojure/java.jdbc "0.2.2"]]
